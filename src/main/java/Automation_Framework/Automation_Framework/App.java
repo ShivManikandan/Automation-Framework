605ffd0cf6d3560bde4759b4d9a2f42dcb713ccf
Manikandan <you@example.com>
2020-01-25 13:28:45 +0530

    my first checkin

---
+++ src/main/java/Automation_Framework/Automation_Framework/App.java
@@ -0,0 +1,13 @@
+package Automation_Framework.Automation_Framework;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
